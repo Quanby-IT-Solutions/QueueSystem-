de809a454311de7b9847afea875ba2f5136bc794
ryukkn <126628758+ryukkn@users.noreply.github.com>
2024-11-30 08:55:08 +0800

    cloud

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
     apiKey: '$2y$10$jDyFxPlQt0iz/JmEwFZGSOrCEwlu.GQRdTXT0gIbRlJHD6w2etZbi',
     // QUANBY LAB
-    // api: 'https://quanbylab.com:85/q-api.php',
-    // socket: 'wss://quanbylab.com:3002',
-    // nodeserver: 'https://quanbylab.com:3002',
-    // server: 'https://quanbylab.com/public/',
+    api: 'https://quanbylab.com:85/q-api.php',
+    socket: 'wss://quanbylab.com:3002',
+    nodeserver: 'https://quanbylab.com:3002',
+    server: 'https://quanbylab.com/public/',
     
     // PCSO 
     // api: 'https://vsu.quanby-staging.com/apiSQL.php',
@@ -13,10 +13,10 @@ export const environment = {
     // server: 'https://vsu.quanby-staging.com/public/',
 
      // LOCAL 
-     api: 'https://vsu-qsm.gov.ph/api.php',
-     socket: 'wss://vsu-qsm.gov.ph/websocket',
-     nodeserver: 'https://vsu-qsm.gov.ph',
-     server: 'https://vsu-qsm.gov.ph/public/',
+    //  api: 'https://vsu-qsm.gov.ph/api.php',
+    //  socket: 'wss://vsu-qsm.gov.ph/websocket',
+    //  nodeserver: 'https://vsu-qsm.gov.ph',
+    //  server: 'https://vsu-qsm.gov.ph/public/',
 
     // api: 'http://192.168.10.2:85/api.php',
     // socket: 'ws://192.168.10.2:3000',
